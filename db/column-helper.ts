f1092a3654baddd434ef102ddfc6d43f7c4f58b6
Yu-Kashiro <poyonnnnnn@gmail.com>
2026-01-04 15:16:18 +0900

    feat: column-helperとget-base-urlの新しいファイルを追加

---
+++ db/column-helper.ts
@@ -0,0 +1,18 @@
+import { sql } from "drizzle-orm";
+import { text } from "drizzle-orm/sqlite-core";
+import { integer } from "drizzle-orm/sqlite-core";
+import { nanoid } from "nanoid";
+
+export const id = text("id")
+  .primaryKey()
+  .$defaultFn(() => nanoid(10));
+
+export const timestamps = {
+  createdAt: integer("created_at", { mode: "timestamp_ms" })
+    .default(sql`(cast(unixepoch('subsecond') * 1000 as integer))`)
+    .notNull(),
+  updatedAt: integer("updated_at", { mode: "timestamp_ms" })
+    .default(sql`(cast(unixepoch('subsecond') * 1000 as integer))`)
+    .$onUpdate(() => /* @__PURE__ */ new Date())
+    .notNull(),
+};
