48e4af0527ac6af11db827424703027863fa052e
Yu-Kashiro <poyonnnnnn@gmail.com>
2026-01-05 18:13:26 +0900

    feat: Homeコンポーネントをニュースアイテムを受け取るように変更し、表示内容を更新

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDateJa(date: Date): string {
+  return date.toLocaleDateString("ja-JP", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  })
+}
